6c0a0e77f95bd51db8ed9334269d46d9e701e906
Cédric Belin <cedric@belin.io>
2018-12-02 04:21:25 +0100

    Added a build system based on Grinder

---
+++ tool/grind.dart
@@ -0,0 +1,53 @@
+import 'package:grinder/grinder.dart';
+import 'package:nodejs_interop/globals.dart';
+
+/// Starts the build system.
+Future<void> main(List<String> args) => grind(args);
+
+@DefaultTask('Builds the project')
+void build() => Pub.run('build_runner', arguments: ['build', '--delete-conflicting-outputs']);
+
+@Task('Deletes all generated files and reset any saved state')
+void clean() {
+  defaultClean();
+  ['.dart_tool/build', 'doc/api', webDir.path].map(getDir).forEach(delete);
+  FileSet.fromDir(getDir('var'), pattern: '!.*', recurse: true).files.forEach(delete);
+}
+
+@Task('Uploads the results of the code coverage')
+void coverage() => Pub.run('coveralls', arguments: const ['var/lcov.info']);
+
+@Task('Builds a redistributable package')
+Future<void> dist() async {
+  Dart2js.compile(joinFile(binDir, ['reverse_proxy.dart']), minify: true);
+  final output = joinFile(binDir, ['reverse_proxy.js']);
+  return output.writeAsString('${getPreamble(minified: true)}\n${await output.readAsString()}');
+}
+
+@Task('Builds the documentation')
+Future<void> doc() async {
+  await getFile('CHANGELOG.md').copy('doc/about/changelog.md');
+  await getFile('LICENSE.md').copy('doc/about/license.md');
+  DartDoc.doc();
+  run('mkdocs', arguments: ['build']);
+}
+
+@Task('Fixes the coding standards issues')
+void fix() => DartFmt.format(existingSourceDirs, lineLength: 200);
+
+@Task('Performs the static analysis of source code')
+void lint() => Analyzer.analyze(existingSourceDirs);
+
+@Task('Runs the test suites')
+void test() => TestRunner().test(platformSelector: 'node');
+
+@Task('Upgrades the project to the latest revision')
+void upgrade() {
+  run('git', arguments: ['reset', '--hard']);
+  run('git', arguments: ['fetch', '--all', '--prune']);
+  run('git', arguments: ['pull', '--rebase']);
+  Pub.upgrade();
+}
+
+@Task('Watches for file changes')
+void watch() => Pub.run('build_runner', arguments: ['watch', '--delete-conflicting-outputs']);
